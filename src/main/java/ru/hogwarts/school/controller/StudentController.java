c72e0d2bfdd7eb241ceeca8f26d2f7a5ed71926e
iskrot <olga.volwskaya@gmail.com>
2025-06-10 14:10:37 +0300

    сделала домашнее задание

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,49 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.model.service.StudentService;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        studentService.add(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable(name = "id") long id){
+        return studentService.get(id);
+    }
+
+    @PutMapping
+    public void putStudent(@RequestBody Student student){
+        studentService.put(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable(name = "id") long id){
+        studentService.remove(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Student>> findStudents(@RequestParam(required = false) int age) {
+        if (age > 0) {
+            return ResponseEntity.ok(studentService.findByAge(age));
+        }
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+
+}
